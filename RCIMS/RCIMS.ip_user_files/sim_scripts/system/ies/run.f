858b989a0a9fd6fc67ea3f10bd0ccca150b3c537
Jingbo Gao <gaojingbo0620@outlook.com>
2017-11-18 13:21:10 +0800

    Create DAC hardware structure and software prototype

---
+++ RCIMS/RCIMS.ip_user_files/sim_scripts/system/ies/run.f
@@ -0,0 +1,100 @@
+-makelib ies/xil_defaultlib -sv \
+  "C:/Xilinx/Vivado/2017.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+-endlib
+-makelib ies/xpm \
+  "C:/Xilinx/Vivado/2017.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies/axi_infrastructure_v1_1_0 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/7e3a/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib ies/xil_common_vip_v1_0_0 -sv \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/2ad9/hdl/xil_common_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies/smartconnect_v1_0 -sv \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/39ca/hdl/sc_util_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies/axi_protocol_checker_v1_1_14 -sv \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/a1b2/hdl/axi_protocol_checker_v1_1_vl_rfs.sv" \
+-endlib
+-makelib ies/axi_vip_v1_0_2 -sv \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/6eb1/hdl/axi_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies/axi_vip_v1_0_1 -sv \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/1d61/hdl/processing_system7_vip_v1_0_vl_rfs.sv" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/system/ip/system_processing_system7_0_0/sim/system_processing_system7_0_0.v" \
+-endlib
+-makelib ies/xbip_utils_v3_0_7 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/3d01/hdl/xbip_utils_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/axi_utils_v2_0_3 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/43f5/hdl/axi_utils_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xbip_pipe_v3_0_3 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/7db8/hdl/xbip_pipe_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xbip_bram18k_v3_0_3 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/403d/hdl/xbip_bram18k_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/mult_gen_v12_0_12 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/f0ab/hdl/mult_gen_v12_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xbip_dsp48_wrapper_v3_0_4 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/1e87/hdl/xbip_dsp48_wrapper_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xbip_dsp48_addsub_v3_0_3 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/c9c4/hdl/xbip_dsp48_addsub_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xbip_dsp48_multadd_v3_0_3 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/779d/hdl/xbip_dsp48_multadd_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/dds_compiler_v6_0_13 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/5753/hdl/dds_compiler_v6_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/system/ip/system_dds_compiler_0_1/sim/system_dds_compiler_0_1.vhd" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/system/ipshared/2317/hdl/DDS_v1_0_S00_AXI.v" \
+  "../../../bd/system/ipshared/2317/hdl/DDS_v1_0.v" \
+  "../../../bd/system/ip/system_DDS_0_0/sim/system_DDS_0_0.v" \
+-endlib
+-makelib ies/lib_cdc_v1_0_2 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/52cb/hdl/lib_cdc_v1_0_rfs.vhd" \
+-endlib
+-makelib ies/proc_sys_reset_v5_0_11 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/5db7/hdl/proc_sys_reset_v5_0_vh_rfs.vhd" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/system/ip/system_rst_ps7_0_50M_0/sim/system_rst_ps7_0_50M_0.vhd" \
+-endlib
+-makelib ies/generic_baseblocks_v2_1_0 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/f9c1/hdl/generic_baseblocks_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/fifo_generator_v13_1_4 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/ebc2/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib ies/fifo_generator_v13_1_4 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/ebc2/hdl/fifo_generator_v13_1_rfs.vhd" \
+-endlib
+-makelib ies/fifo_generator_v13_1_4 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/ebc2/hdl/fifo_generator_v13_1_rfs.v" \
+-endlib
+-makelib ies/axi_data_fifo_v2_1_12 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/95b9/hdl/axi_data_fifo_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/axi_register_slice_v2_1_13 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/55c0/hdl/axi_register_slice_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/axi_protocol_converter_v2_1_13 \
+  "../../../../RCIMS.srcs/sources_1/bd/system/ipshared/f0ae/hdl/axi_protocol_converter_v2_1_vl_rfs.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../bd/system/ip/system_auto_pc_0/sim/system_auto_pc_0.v" \
+  "../../../bd/system/hdl/system.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  glbl.v
+-endlib
+
