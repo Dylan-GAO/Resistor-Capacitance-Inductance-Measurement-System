fd59f788e27c98f50a970f19ad9d5fe2a49b659d
Jingbo Gao <gaojingbo0620@outlook.com>
2017-12-14 21:39:21 +0800

    Project Over

--- RCIMS/RCIMS.runs/impl_1/RCIMS_bd_wrapper.tcl
+++ RCIMS/RCIMS.runs/impl_1/RCIMS_bd_wrapper.tcl
@@ -42,7 +42,6 @@ proc step_failed { step } {
   close $ch
 }
 
-set_msg_config -id {Common 17-41} -limit 10000000
 set_msg_config -id {HDL-1065} -limit 10000
 
 start_step init_design
@@ -52,26 +51,26 @@ set rc [catch {
   create_project -in_memory -part xc7z010clg400-1
   set_property design_mode GateLvl [current_fileset]
   set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.cache/wt [current_project]
-  set_property parent.project_path C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.xpr [current_project]
+  set_property webtalk.parent_dir C:/work/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.cache/wt [current_project]
+  set_property parent.project_path C:/work/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.xpr [current_project]
   set_property ip_repo_paths {
-  C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/ip_repo
-  C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/Display_1.0
-  C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/Display_1.0/src/lcd_display_0
-  C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/Display_1.0/src/lcd_display_0_1
-  C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/ADC_1.0
-  C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/DDS_1.0
+  C:/work/Resistor-Capacitance-Inductance-Measurement-System/ip_repo
+  C:/work/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/Display_1.0
+  C:/work/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/Display_1.0/src/lcd_display_0
+  C:/work/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/Display_1.0/src/lcd_display_0_1
+  C:/work/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/ADC_1.0
+  C:/work/Resistor-Capacitance-Inductance-Measurement-System/ip_repo/DDS_1.0
 } [current_project]
-  set_property ip_output_repo C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.cache/ip [current_project]
+  set_property ip_output_repo C:/work/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.cache/ip [current_project]
   set_property ip_cache_permissions {read write} [current_project]
   set_property XPM_LIBRARIES XPM_CDC [current_project]
-  add_files -quiet C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.runs/synth_1/RCIMS_bd_wrapper.dcp
+  add_files -quiet C:/work/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.runs/synth_1/RCIMS_bd_wrapper.dcp
   set_msg_config -source 4 -id {BD 41-1661} -suppress
   set_param project.isImplRun true
-  add_files C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.srcs/sources_1/bd/RCIMS_bd/RCIMS_bd.bd
-  set_property is_locked true [get_files C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.srcs/sources_1/bd/RCIMS_bd/RCIMS_bd.bd]
+  add_files C:/work/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.srcs/sources_1/bd/RCIMS_bd/RCIMS_bd.bd
+  set_property is_locked true [get_files C:/work/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.srcs/sources_1/bd/RCIMS_bd/RCIMS_bd.bd]
   set_param project.isImplRun false
-  read_xdc C:/Users/Administrator/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.srcs/constrs_1/new/system.xdc
+  read_xdc C:/work/Resistor-Capacitance-Inductance-Measurement-System/RCIMS/RCIMS.srcs/constrs_1/new/system.xdc
   set_param project.isImplRun true
   link_design -top RCIMS_bd_wrapper -part xc7z010clg400-1
   set_param project.isImplRun false
--- RCIMS/RCIMS.srcs/sources_1/bd/RCIMS_bd/hdl/RCIMS_bd.v
+++ RCIMS/RCIMS.srcs/sources_1/bd/RCIMS_bd/hdl/RCIMS_bd.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2017.2 (win64) Build 1909853 Thu Jun 15 18:39:09 MDT 2017
-//Date        : Wed Dec 13 15:53:18 2017
-//Host        : SC-201709270320 running 64-bit major release  (build 9200)
+//Date        : Thu Dec 14 18:22:57 2017
+//Host        : DESKTOP-GLIA512 running 64-bit major release  (build 9200)
 //Command     : generate_target RCIMS_bd.bd
 //Design      : RCIMS_bd
 //Purpose     : IP block netlist
